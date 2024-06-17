dee75c5d8deaee57ec3075f84308d471fbd387d9
grandeljay <github@grandels.email>
2024-06-17 13:10:02 +0200

    chore(module): update to v1.3.1

--- src-mmlc/Constants.php
+++ src-mmlc/Constants.php
@@ -4,7 +4,7 @@ namespace Grandeljay\ShippingConditions;
 
 class Constants
 {
-    public const MODULE_VERSION           = '1.3.0';
+    public const MODULE_VERSION           = '1.3.1';
     public const MODULE_CHECKOUT_NAME     = 'MODULE_CHECKOUT_GRANDELJAY_SHIPPING_CONDITIONS_CHECKOUT';
     public const MODULE_CHECKOUT_FILENAME = \grandeljay_shipping_conditions_checkout::class . '.php';
 }
